d65dca675dd98e1c33a38edd778c641c0dc46ac7
김동호 <saromeokdh@gmail.com>
2023-05-29 14:50:22 +0900

    Add files via upload

---
+++ work.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $('.myimg').show();
+    $('.myimg2').hide();
+
+    $('.myimg').click(function(){
+        $('.myimg').hide();
+        $('.myimg2').show();
+    });
+    $('.myimg2').click(function(){
+        $('.myimg').show();
+        $('.myimg2').hide();
+    });
+    $('a').hover(function(){
+        $(this).css('color','red');
+    }, function(){
+        $('.acolor').css('color','#F361DC');
+    });
+
+    $('.emailfont').hover(function(){
+        $(this).css('color','red');
+    }, function(){
+        $(this).css('color','#A6A6A6')
+    });
+    $('.pdfcolor').hover(function(){
+        $(this).css('color','black');
+    }, function(){
+        $(this).css('color','gray')
+    });
+    $('#panel1').hide();
+        $('.2021').click(function(){
+            $(this).next('#panel1').slideToggle();
+        });
+    $('#panel2').hide();
+        $('.2022').click(function(){
+            $(this).next('#panel2').slideToggle();
+        });
+    if(window.location.href === 'https://noobkdh.github.io/Introduce_homepage/home.html'){
+        $('.home').css('font-weight','bold');
+    };
+    if(window.location.href === 'https://noobkdh.github.io/Introduce_homepage/introduce.html'){
+        $('.introduce').css('font-weight','bold');
+    };
+    if(window.location.href==='https://noobkdh.github.io/Introduce_homepage/project.html'){
+        $('.project').css('font-weight','bold');
+    };
+
+});
