d1c3e9840378bf503a5573ef4bca924baab9bf09
김동호 <saromeokdh@gmail.com>
2023-05-29 15:04:44 +0900

    Update work.js

--- work.js
+++ work.js
@@ -34,13 +34,13 @@ $(document).ready(function(){
         $('.2022').click(function(){
             $(this).next('#panel2').slideToggle();
         });
-    if(window.location.href === 'https://noobkdh.github.io/Introduce_homepage/home.html'){
+    if(window.location.href === './home.html'){
         $('.home').css('font-weight','bold');
     };
-    if(window.location.href === 'https://noobkdh.github.io/Introduce_homepage/introduce.html'){
+    if(window.location.href === './introduce.html'){
         $('.introduce').css('font-weight','bold');
     };
-    if(window.location.href==='https://noobkdh.github.io/Introduce_homepage/project.html'){
+    if(window.location.href==='./project.html'){
         $('.project').css('font-weight','bold');
     };
 
